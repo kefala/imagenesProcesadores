687736137fcf10776a3d7cfa71cbfba24660d47b
kefala <martin.g.lobo@gmail.com>
2016-06-13 23:34:51 -0300

    Image entity

---
+++ src/AppBundle/Controller/ImageController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Image;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Image controller.
+ *
+ * @Route("/images")
+ */
+class ImageController extends Controller
+{
+    /**
+     * Lists all Image entities.
+     *
+     * @Route("/", name="images_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $images = $em->getRepository('AppBundle:Image')->findAll();
+
+        return $this->render('image/index.html.twig', array(
+            'images' => $images,
+            ));
+    }
+
+    /**
+     * Finds and displays a Image entity.
+     *
+     * @Route("/{id}", name="images_show")
+     * @Method("GET")
+     */
+    public function showAction(Image $image)
+    {
+
+        return $this->render('image/show.html.twig', array(
+            'image' => $image,
+            ));
+    }
+
+    /**
+     * Finds and displays a Image entity.
+     *
+     * @Route("/api/{id}", name="api_create")
+     * @Method("GET")
+     */
+    public function apiImage($id)
+    {
+        $response = new JsonResponse();
+        $em = $this->getDoctrine()->getManager();
+
+        $image = $em->getRepository('AppBundle:Image')->find($id);
+
+        if (!empty($image)) {
+            $response->setData(array(
+                'nombre' => $image->getName(),
+                'path' => $image->getUrl()
+            ));
+        } else {
+            $response->setData(array());
+        }
+        
+        
+
+        return $response;
+    }
+
+    /**
+     * Finds and displays a Image entity.
+     *
+     * @Route("/api/image/create/{name}/{url}", name="create_image")
+     * @Method("GET")
+     */
+    public function addImage($name, $url)
+    {
+        $response = new JsonResponse();
+        $em = $this->getDoctrine()->getManager();
+
+        $images = $em->getRepository('AppBundle:Image')->findAll();
+
+        if (sizeof($images) < 8) {
+            $image = new Image();
+            $image->setName($name);
+            $image->setUrl($url);
+            
+            $em->persist($image);
+            $em->flush();
+
+            $response->setData(array(
+                'data' => $image->getId()
+            ));  
+        } else {
+            $response->setData(array());  
+        }
+        
+        return $response;
+    }
+
+}
---
+++ src/AppBundle/Entity/Image.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Image
+ *
+ * @ORM\Table(name="image")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ImageRepository")
+ */
+class Image
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Image
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Image
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
+
